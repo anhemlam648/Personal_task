3af669a997fab00655f97750dd5e2f8c6ce6fd73
VuTrungNghia <vunghia467@gmail.com>
2024-08-03 10:39:45 +0700

    updateRegister

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextField, Button, Container, Typography } from '@material-ui/core';
+import { TextField, Button, Container, Typography } from '@mui/material';
 
 const Register = () => {
   return (
